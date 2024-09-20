b9b56efa4ec3eded7bbb8e8df32e452fe086a513
MollyTSlater <mollytslater@gmail.com>
2024-09-19 21:52:41 -0400

    Update Storage.java

--- src/Storage.java
+++ src/Storage.java
@@ -2,9 +2,10 @@
 public interface Storage{
 	public default void storage(StoreData data)
 	{
-		// store data
-		StorageAddResponse storeData = data.storeData(new DataStorageRequest());
 		
-		StorageLoadResponse loadData = data.loadData(storeData.getDataKey());
+		// store data
+		//StorageAddResponse storeData = data.storeData(new DataStorageRequest());
+		
+		//StorageLoadResponse loadData = data.loadData(storeData.getDataKey());
 	}
 }
