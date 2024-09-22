670112fb8179db0de8137dd24fd00b43aebed7dd
KieranH-1 <heatonk1@newpaltz.edu>
2024-09-22 19:34:36 -0400

    Add files via upload

--- src/PrototypeCompute.java
+++ src/PrototypeCompute.java
@@ -1,19 +1,7 @@
 
 public class PrototypeCompute {
-	public void prototypeComputer(ReaderWriter readerWriter, Computer computer) {
-		//initialize program
-		InitializeResponse initalizeResponse = readerWriter.initialize(new InitializeRequest());
-		
-		//read input number and delimiters
-		ComputerInputResponse computerInputResponse = readerWriter.readInput(new ComputerInputRequest());
-		
+	public void prototypeComputer(Computer computer) {		
 		//compute output
-		InputNumber inputNumber = computerInputResponse.getInputNumber();
-		ComputeResponse computeResponse = computer.computeOutput(inputNumber);
-		
-		//get output and write down output number
-		Delimiter inputOutputDelim = computerInputResponse.getInputOutputDelim();
-		Delimiter outputInputDelim = computerInputResponse.getOutputInputDelim();
-		ComputerOutputResponse computerOutputResponse = readerWriter.writeOutput(computeResponse.getOutput(), inputOutputDelim, outputInputDelim);		
+		String result = computer.compute(1, ':', ';');		
 	}
 }
