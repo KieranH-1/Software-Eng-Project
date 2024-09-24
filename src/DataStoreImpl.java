f8a7661d84e2cb2fe6585564b9e3a5c1d4b0e841
MollyTSlater <mollytslater@gmail.com>
2024-09-24 17:59:07 -0400

    DataStore Implementation

---
+++ src/DataStoreImpl.java
@@ -0,0 +1,15 @@
+// just chilling for rn
+
+public class DataStoreImpl implements DataStore{
+	//InputConfig input = null;
+	//OutputConfig output = null;
+	//String result = " ";
+	
+	public Iterable<Integer> read(InputConfig input) {
+		return null;
+	}
+
+	public WriteResult appendSingleResult(OutputConfig output, String result) {
+		return null;
+	}
+}
