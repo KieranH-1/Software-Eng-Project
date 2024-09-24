418e304fa8c26e246c33a5554d9d267ef5d670ff
jasonmontas <jason.montas24@gmail.com>
2024-09-24 16:45:14 -0400

    Add files via upload

---
+++ src/ComputationCoordinatorTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+
+public class ComputationCoordinatorTest {
+
+   @Test
+    public void testCompute() {
+        InputConfig mockInputConfig = Mockito.mock(InputConfig.class);
+        OutputConfig mockOutputConfig = Mockito.mock(OutputConfig.class);
+        ComputationCoordinator mockCC = Mockito.mock(ComputationCoordinator.class);
+        ComputeRequest request = new ComputeRequest(mockInputConfig, mockOutputConfig);
+
+        ComputeResult result = mockCC.compute(request);
+        if(!result.equals("one")) {
+            fail();
+        }
+    }
+}
