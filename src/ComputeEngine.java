7d37c17d9f2b8f80a1fcc31c4e2c021bd5807e22
KieranH-1 <heatonk1@newpaltz.edu>
2024-09-24 16:47:57 -0400

    Add files via upload

--- src/ComputeEngine.java
+++ src/ComputeEngine.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * API 3: Conceptual API within the compute engine process
  * 
@@ -17,5 +19,5 @@
  * from the system specification that it will always be an int, it's fine to just use that as well.
  */
 public interface ComputeEngine {
-	String compute(int value);
+	String compute(List<Integer> input);
 }
