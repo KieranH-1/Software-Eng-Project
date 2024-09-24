e3f309d03f11ba3867958939299788048cf1f231
KieranH-1 <heatonk1@newpaltz.edu>
2024-09-24 16:47:39 -0400

    Add files via upload

---
+++ src/Computer.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Computer implements ComputeEngine {
+	
+	@Override
+	public String compute(List<Integer> input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
