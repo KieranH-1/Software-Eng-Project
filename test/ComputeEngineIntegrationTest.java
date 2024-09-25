3b2591881b76ad8fb1547223a3daa1272a700455
KieranH-1 <heatonk1@newpaltz.edu>
2024-09-24 22:41:35 -0400

    Add files via upload

--- test/ComputeEngineIntegrationTest.java
+++ test/ComputeEngineIntegrationTest.java
@@ -1,23 +1,22 @@
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import static org.mockito.Mockito.when;
-
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.mockito.Mockito.any;
 
 public class ComputeEngineIntegrationTest {
 
 	@Test
 	public void testCompute() {
 		Computer testComputer = new Computer();
+		DataStore dataStoreImpl = new DataStoreImpl();
 		List<Integer> input = new ArrayList<>();
 		
 		input.add(1);
 		input.add(10);
 		input.add(25);
+		InMemoryInputConfig inputConfig = new InMemoryInputConfig(input);
+		
+		testComputer.compute(dataStoreImpl.read(inputConfig));
 		
 		if (testComputer.compute(input) != "1, one; 10, ten; 25, twenty-five") {
 			fail();
