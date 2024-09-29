ae3b3e6694fbfb65d755f426d3f1396ac817c729
MollyTSlater <mollytslater@gmail.com>
2024-09-29 17:53:41 -0400

    Update TestDataStorePrototype.java

--- test/TestDataStorePrototype.java
+++ test/TestDataStorePrototype.java
@@ -5,30 +5,22 @@ import static org.mockito.Mockito.when;
 
 public class TestDataStorePrototype {
 	@Test
-	public void testDataStore(){
+	public void testDataStoreRead(){
 // in progress working thru issues 
 		InputConfig mockInput = Mockito.mock(InputConfig.class);
+		
+		DataStore mockDataStore = new DataStoreImpl();
+		Assertions.assertEquals(1L, dataStore.read(mockInput));
+		//WriteResult mockWriteResult = Mockito.mock(WriteResult.class);
+
+		//Iterable<Integer> loadData = mockDataStore.read(mockInput);
+		
+	}
+
+	@Test
+	public void testDataStoreWrite(){
 		OutputConfig mockOutput = Mockito.mock(OutputConfig.class);
-		DataStore mockDataStore = Mockito.mock(DataStore.class);
-		WriteResult mockWriteResult = Mockito.mock(WriteResult.class);
-
-		Iterable<Integer> loadData = mockDataStore.read(mockInput);
-		//WriteResult mockAppend = new appendSingleResult(mockOutput, result);
-
-		
-		if (loadData == null) {
-            		fail();
-		}
-		
-		String result = "result";
-        	when(mockDataStore.appendSingleResult(mockOutput, result)).thenReturn(mockWriteResult);
-
-		WriteResult mockAppend = mockDataStore.appendSingleResult(mockOutput, result);
-        
-       		if (mockAppend == null) {
-            		fail("mockAppend should not be null");
-        	}
-		
-		
+		DataStore mockDataStore = new DataStoreImpl();
+		Assertions.assertEquals(WriteResultStatus.SUCCESS, dataStore.appendSingleResult(outputConfig, "result").getStatus());
 	}
 }
