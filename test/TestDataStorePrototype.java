b583f797311d292ca618f3b47a1a3f17923e6a40
MollyTSlater <mollytslater@gmail.com>
2024-09-29 17:57:50 -0400

    Update TestDataStorePrototype.java
    Updated to match prof's code

--- test/TestDataStorePrototype.java
+++ test/TestDataStorePrototype.java
@@ -1,7 +1,6 @@
-import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.when;
 
 public class TestDataStorePrototype {
 	@Test
