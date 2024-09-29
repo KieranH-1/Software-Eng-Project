1291bad57fac4f37aa06152a683cd19691c6ce06
KieranH-1 <heatonk1@newpaltz.edu>
2024-09-29 19:49:06 -0400

    Update TestDataStore.java

--- test/TestDataStore.java
+++ test/TestDataStore.java
@@ -8,14 +8,14 @@ public class TestDataStore implements DataStore {
 	public Iterable<Integer> read(InputConfig input) {
 		// Test code is allowed to assume it's getting the right types; this will fail with a ClassCastException if it gets
 		// another type of input. For production code, we'd want some better user input validation
-		return ((InMemoryInputConfig)input).getInputs();
+		return ((InMemoryInputConfig) input).getInputs();
 	}
 
 	@Override
 	public WriteResult appendSingleResult(OutputConfig output, String result) {
 		// Test code is allowed to assume it's getting the right types; this will fail with a ClassCastException if it gets
 		// another type of input. For production code, we'd want some better user input validation
-		((InMemoryOutputConfig)output).getOutputMutable().add(result);
+		((InMemoryOutputConfig) output).getOutputMutable().add(result);
 		return () -> WriteResult.WriteResultStatus.SUCCESS;
 	}
 
