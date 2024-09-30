a01cdf06f18da2391c11a38e1c7719b68698e240
KieranH-1 <heatonk1@newpaltz.edu>
2024-09-29 23:15:04 -0400

    Update ComputeTest.java

--- test/ComputeTest.java
+++ test/ComputeTest.java
@@ -5,11 +5,20 @@ import org.junit.jupiter.api.Test;
 public class ComputeTest {
 
 	@Test
-	public void smokeTestCompute() {
+	public void smokeTestCompute1() {
 		// The computation component has very simple inputs/outputs and no dependencies, so we can
 		// write a smoke test with no mocks at all
 		
 		ComputeEngine engine = new ComputeEngineImpl();
 		Assertions.assertEquals("nine-hundred ninety nine", engine.compute(999));
+	}
+	
+	@Test
+	public void smokeTestCompute2() {
+		// The computation component has very simple inputs/outputs and no dependencies, so we can
+		// write a smoke test with no mocks at all
+		
+		ComputeEngine engine = new ComputeEngineImpl();
+		Assertions.assertEquals("zero", engine.compute(0));
 	}
 }
