3e9718ec0268e9ab92ca06356ddf1725e0f2392d
KieranH-1 <heatonk1@newpaltz.edu>
2024-09-29 20:19:26 -0400

    Update DataStoreTest.java

--- test/DataStoreTest.java
+++ test/DataStoreTest.java
@@ -3,8 +3,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import edu.softwareeng.sample.WriteResult.WriteResultStatus;
-
 /*
  * In this case, I've split out the smoke tests into two tests, one per method. It's also fine to consolidate
  * these into a single test
@@ -32,6 +30,6 @@ public class DataStoreTest {
 		// assertEquals will use .equals, so this type of call is generally safe for any Object, but for enums you can also use ==
 		// Note that if your dummy implementation returns null, this will NPE - that's fine! As long as the test fails,
 		// the exact failure doesn't matter
-		Assertions.assertEquals(WriteResultStatus.SUCCESS, dataStore.appendSingleResult(outputConfig, "result").getStatus());
+		Assertions.assertEquals(WriteResult.WriteResultStatus.SUCCESS, dataStore.appendSingleResult(outputConfig, "result").getStatus());
 	}
 }
