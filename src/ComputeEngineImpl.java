8fcaaf8c5967abe963068240f8aaede09b63a278
KieranH-1 <heatonk1@newpaltz.edu>
2024-09-29 22:51:09 -0400

    Update ComputeEngineImpl.java

--- src/ComputeEngineImpl.java
+++ src/ComputeEngineImpl.java
@@ -16,229 +16,100 @@ public class ComputeEngineImpl implements ComputeEngine {
 			currentChar = valueString.charAt(i);
 			if (i == 0 && length == 3) {
 				if (currentChar == '0') {
-<<<<<<< Updated upstream
-					valueAsWord += "";}
+					valueAsWord += ""; }
 				if (currentChar == '1') { 
-					valueAsWord += "one-hundred";}
+					valueAsWord += "one-hundred"; }
 				if (currentChar == '2') {
-					valueAsWord += "two-hundred";}
+					valueAsWord += "two-hundred"; }
 				if (currentChar == '3') {
-					valueAsWord += "three-hundred";}
+					valueAsWord += "three-hundred"; }
 				if (currentChar == '4') {
-					valueAsWord += "four-hundred";}
+					valueAsWord += "four-hundred"; }
 				if (currentChar == '5') {
-					valueAsWord += "five-hundred";}
+					valueAsWord += "five-hundred"; }
 				if (currentChar == '6') {
-					valueAsWord += "six-hundred";}
+					valueAsWord += "six-hundred"; }
 				if (currentChar == '7') {
-					valueAsWord += "seven-hundred";}
+					valueAsWord += "seven-hundred"; }
 				if (currentChar == '8') {
-					valueAsWord += "eight-hundred";}
+					valueAsWord += "eight-hundred"; }
 				if (currentChar == '9') {
-					valueAsWord += "nine-hundred";}
-=======
-					valueAsWord += "";
-				}
-				if (currentChar == '1') { 
-					valueAsWord += "one-hundred";
-				}
-				if (currentChar == '2') {
-					valueAsWord += "two-hundred";
-				}
-				if (currentChar == '3') {
-					valueAsWord += "three-hundred";
-				}
-				if (currentChar == '4') {
-					valueAsWord += "four-hundred";
-				}
-				if (currentChar == '5') {
-					valueAsWord += "five-hundred";
-				}
-				if (currentChar == '6') {
-					valueAsWord += "six-hundred";
-				}
-				if (currentChar == '7') {
-					valueAsWord += "seven-hundred";
-				}
-				if (currentChar == '8') {
-					valueAsWord += "eight-hundred";
-				}
-				if (currentChar == '9') {
-					valueAsWord += "nine-hundred";
-				}
->>>>>>> Stashed changes
+					valueAsWord += "nine-hundred"; }
 				valueAsWord += " ";
 			}
 			if ((i == 0 && length == 2) || (i == 1 && length == 3)) {
 				if (currentChar == '0') {
-<<<<<<< Updated upstream
-					valueAsWord += "";}
-=======
 					valueAsWord += "";
 				}
->>>>>>> Stashed changes
 				if (currentChar == '1') {
 					
 					nextChar = valueString.charAt(i + 1);
 					
 					if(nextChar == '0') {
-<<<<<<< Updated upstream
-						valueAsWord += "ten";}
+						valueAsWord += "ten"; }
 					if(nextChar == '1') {
-						valueAsWord += "eleven";}
+						valueAsWord += "eleven"; }
 					if(nextChar == '2') {
-						valueAsWord += "twelve";}
+						valueAsWord += "twelve"; }
 					if(nextChar == '3') {
-						valueAsWord += "thirteen";}
+						valueAsWord += "thirteen"; }
 					if(nextChar == '4') {
-						valueAsWord += "fourteen";}
+						valueAsWord += "fourteen"; }
 					if(nextChar == '5') {
-						valueAsWord += "fifteen";}
+						valueAsWord += "fifteen"; }
 					if(nextChar == '6') {
-						valueAsWord += "sixteen";}
+						valueAsWord += "sixteen"; }
 					if(nextChar == '7') {
-						valueAsWord += "seventeen";}
+						valueAsWord += "seventeen"; }
 					if(nextChar == '8') {
-						valueAsWord += "eighteen";}
+						valueAsWord += "eighteen"; }
 					if(nextChar == '9') {
-						valueAsWord += "nineteen";}
-=======
-						valueAsWord += "ten";
-					}
-					if(nextChar == '1') {
-						valueAsWord += "eleven";
-					}
-					if(nextChar == '2') {
-						valueAsWord += "twelve";
-					}
-					if(nextChar == '3') {
-						valueAsWord += "thirteen";
-					}
-					if(nextChar == '4') {
-						valueAsWord += "fourteen";
-					}
-					if(nextChar == '5') {
-						valueAsWord += "fifteen";
-					}
-					if(nextChar == '6') {
-						valueAsWord += "sixteen";
-					}
-					if(nextChar == '7') {
-						valueAsWord += "seventeen";
-					}
-					if(nextChar == '8') {
-						valueAsWord += "eighteen";
-					}
-					if(nextChar == '9') {
-						valueAsWord += "nineteen";
-					}
->>>>>>> Stashed changes
+						valueAsWord += "nineteen"; }
 					
 					i = length;
 				}		
 				if (currentChar == '2') {
-<<<<<<< Updated upstream
-					valueAsWord += "twenty";}
+					valueAsWord += "twenty"; }
 				if (currentChar == '3') {
-					valueAsWord += "thirty";}
+					valueAsWord += "thirty"; }
 				if (currentChar == '4') {
-					valueAsWord += "fourty";}
+					valueAsWord += "fourty"; }
 				if (currentChar == '5') {
-					valueAsWord += "fifty";}
+					valueAsWord += "fifty"; }
 				if (currentChar == '6') {
-					valueAsWord += "sixty";}
+					valueAsWord += "sixty"; }
 				if (currentChar == '7') {
-					valueAsWord += "seventy";}
+					valueAsWord += "seventy"; }
 				if (currentChar == '8') {
-					valueAsWord += "eighty";}
+					valueAsWord += "eighty"; }
 				if (currentChar == '9') {
-					valueAsWord += "ninety";}
-=======
-					valueAsWord += "twenty";
-				}
-				if (currentChar == '3') {
-					valueAsWord += "thirty";
-				}
-				if (currentChar == '4') {
-					valueAsWord += "fourty";
-				}
-				if (currentChar == '5') {
-					valueAsWord += "fifty";
-				}
-				if (currentChar == '6') {
-					valueAsWord += "sixty";
-				}
-				if (currentChar == '7') {
-					valueAsWord += "seventy";
-				}
-				if (currentChar == '8') {
-					valueAsWord += "eighty";
-				}
-				if (currentChar == '9') {
-					valueAsWord += "ninety";
-				}
->>>>>>> Stashed changes
+					valueAsWord += "ninety"; }
 
 				valueAsWord += " ";
 			}
 			if ((length == 1) || (i == 1 && length == 2) || (i == 2 && length == 3)) {
 				if (currentChar == '0') {
-<<<<<<< Updated upstream
-					valueAsWord += "zero";}
+					valueAsWord += "zero"; }
 				if (currentChar == '1') {
-					valueAsWord += "one";}
+					valueAsWord += "one"; }
 				if (currentChar == '2') {
-					valueAsWord += "two";}
+					valueAsWord += "two"; }
 				if (currentChar == '3') {
-					valueAsWord += "three";}
+					valueAsWord += "three"; }
 				if (currentChar == '4') {
-					valueAsWord += "four";}
+					valueAsWord += "four"; }
 				if (currentChar == '5') {
-					valueAsWord += "five";}
+					valueAsWord += "five"; }
 				if (currentChar == '6') {
-					valueAsWord += "six";}
+					valueAsWord += "six"; }
 				if (currentChar == '7') {
-					valueAsWord += "seven";}
+					valueAsWord += "seven"; }
 				if (currentChar == '8') {
-					valueAsWord += "eight";}
+					valueAsWord += "eight"; }
 				if (currentChar == '9') {
-					valueAsWord += "nine";}
-			}
-		}
-			
-=======
-					valueAsWord += "zero";
-				}
-				if (currentChar == '1') {
-					valueAsWord += "one";
-				}
-				if (currentChar == '2') {
-					valueAsWord += "two";
-				}
-				if (currentChar == '3') {
-					valueAsWord += "three";
-				}
-				if (currentChar == '4') {
-					valueAsWord += "four";
-				}
-				if (currentChar == '5') {
-					valueAsWord += "five";
-				}
-				if (currentChar == '6') {
-					valueAsWord += "six";
-				}
-				if (currentChar == '7') {
-					valueAsWord += "seven";
-				}
-				if (currentChar == '8') {
-					valueAsWord += "eight";
-				}
-				if (currentChar == '9') {
-					valueAsWord += "nine";
-				}
+					valueAsWord += "nine"; }
 			}
 		}	
->>>>>>> Stashed changes
 		return valueAsWord;
 	}
 }
