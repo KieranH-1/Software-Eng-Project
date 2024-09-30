aeac12979175b04c0d16dccc693671cf2deed1da
MollyTSlater <mollytslater@gmail.com>
2024-09-29 20:10:05 -0400

    Update DataStoreImpl.java

--- src/DataStoreImpl.java
+++ src/DataStoreImpl.java
@@ -9,7 +9,7 @@ public class DataStoreImpl implements DataStore {
 	public Iterable<Integer> read(InputConfig input) {
 		return Collections.emptyList(); // eventually this will be a stream, but for now always return 0 elements
 	}
-/*
+
 	@Override
 	public WriteResult appendSingleResult(OutputConfig output, String result) {
 		
@@ -18,5 +18,4 @@ public class DataStoreImpl implements DataStore {
 		 
 		return () -> WriteResultStatus.FAILURE; 
 	}
-*/
 }
