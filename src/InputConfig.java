fa382d3c3b2778980f9e1567a8f80b30d975a974
MollyTSlater <mollytslater@gmail.com>
2024-09-29 23:06:39 -0400

    Adding Methods to InputConfig.java

--- src/InputConfig.java
+++ src/InputConfig.java
@@ -1,5 +1,12 @@
 
-
 public interface InputConfig {
+  public List<Integer> inputData;
+  public InputConfig(List<Integer> inputData) {
+        this.inputData = inputData;
+    }
+
+    public List<String> getInputData() {
+        return inputData;
+    }
 
 }
