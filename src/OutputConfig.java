3a21744886817f309979ea46bb552c80886b8a55
MollyTSlater <mollytslater@gmail.com>
2024-09-29 23:09:57 -0400

    Adding methods to OutputConfig.java

--- src/OutputConfig.java
+++ src/OutputConfig.java
@@ -1,4 +1,11 @@
 
 public interface OutputConfig {
+  public List<String> outputData;
+  public InputConfig(List<String> outputData) {
+        this.outputData = outputData;
+    }
 
+    public List<String> getOutputData() {
+        return outputData;
+    }
 }
