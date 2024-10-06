133995e5fb5be898c1cf8b385381ae3db0f02601
MollyTSlater <mollytslater@gmail.com>
2024-10-06 13:04:36 -0400

    Update DataStoreImpl.java

--- src/DataStoreImpl.java
+++ src/DataStoreImpl.java
@@ -2,7 +2,10 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.Collections;
 //import WriteResult.WriteResultStatus;
 
@@ -10,7 +13,22 @@ public class DataStoreImpl implements DataStore {
 
 	@Override
 	public Iterable<Integer> read(InputConfig input) {
-		
+		List<Integer> userIntegers = new ArrayList<>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(input.getFileName() ) ) ) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				try {
+					userIntegers.add(Integer.parseInt(line.trim()));
+				} catch (NumberFormatException e) {
+					System.err.println("Integer is invalid :( :" + line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return userIntegers;
 	}
 
 	@Override
