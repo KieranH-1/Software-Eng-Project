a6484398147e03aaa5652775c11bfbcf7cd33fd1
MollyTSlater <mollytslater@gmail.com>
2024-10-06 12:46:56 -0400

    Update InputConfig.java
    Adding fileName variable and 2 method

--- src/InputConfig.java
+++ src/InputConfig.java
@@ -1,4 +1,11 @@
 public interface InputConfig {
-	
+	private String fileName;
+
+	public InputConfig(String fileName) {
+		this.fileName = fileName;
+    	}
+	public String getFileName() {
+        	return fileName;
+    	}
 }
 
