6c0d5dcb685414c943d72e90ab561e8f69e6230e
MollyTSlater <mollytslater@gmail.com>
2024-10-06 13:18:57 -0400

    Update InputConfig.java

--- src/InputConfig.java
+++ src/InputConfig.java
@@ -1,11 +1,4 @@
-public class InputConfig {
-	private String fileName;
-
-	public InputConfig(String fileName) {
-		this.fileName = fileName;
-    	}
-	public String getFileName() {
-        	return fileName;
-    	}
+public interface InputConfig {
+	String getFileName();
 }
 
