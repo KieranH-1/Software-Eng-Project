3ce8f8235d0ef64d18e3769247b219a2dd0abaf9
MollyTSlater <mollytslater@gmail.com>
2024-10-07 08:31:41 -0400

    Add void type to first method

--- src/OutputConfig.java
+++ src/OutputConfig.java
@@ -1,6 +1,6 @@
 import java.util.List;
 public interface OutputConfig {
   //public List<String> getOutputMutable();
-  public FileOutputConfig(String fileName);
+  public void FileOutputConfig(String fileName);
   public String getFileName();
 }
