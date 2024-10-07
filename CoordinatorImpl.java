affca4e2c76b9491fccfb7fd0bf9498f947177cb
KieranH-1 <heatonk1@newpaltz.edu>
2024-10-07 09:24:46 -0400

    Add files via upload

---
+++ CoordinatorImpl.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoordinatorImpl implements ComputationCoordinator {
+	
+	private final DataStore ds;
+	private final ComputeEngine ce;
+	
+	public CoordinatorImpl(DataStore ds, ComputeEngine ce) {
+		this.ds = ds;
+		this.ce = ce;
+	}
+
+	@Override
+	public ComputeResult compute(ComputeRequest request) {
+			InputConfig inputConfig = request.getInputConfig();
+			Iterable<Integer> integers = ds.read(inputConfig);
+
+			if (integers == null) {
+				return ComputeResult.FAILURE;
+			}
+
+			try  {
+				OutputConfig outputConfig = request.getOutputConfig();
+				for (Integer integer : integers) {
+					String result = ce.compute(integer);
+				
+					if (result == null) {
+						return ComputeResult.FAILURE;
+					}
+				
+					WriteResult writeResult = ds.appendSingleResult(outputConfig, result);
+					if (writeResult.getStatus() == WriteResult.WriteResultStatus.FAILURE) {
+						return ComputeResult.FAILURE;
+					}
+				}
+			} catch (Exception e) {
+				
+			}
+			return ComputeResult.SUCCESS;
+		}
+	}
+
+
+
