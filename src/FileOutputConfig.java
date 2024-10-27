d47ff2d3c6d5a876cf7af80c8c27c03945c9e707
MollyTSlater <mollytslater@gmail.com>
2024-10-27 07:47:25 -0400

    Added @Override

--- src/FileOutputConfig.java
+++ src/FileOutputConfig.java
@@ -1,10 +1,12 @@
 public class FileOutputConfig implements OutputConfig {
     private String fileName;
 
+    @Override
     public FileOutputConfig(String fileName) {
         this.fileName = fileName;
     }
 
+    @Override
     public String getFileName() {
         return fileName;
     }
