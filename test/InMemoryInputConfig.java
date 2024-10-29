a91c1c03c0cf3e806c1077bb1aee09501dab557f
MollyTSlater <mollytslater@gmail.com>
2024-10-29 16:42:21 -0400

    Adding fileName info to InMemoryInputConfig.java

--- test/InMemoryInputConfig.java
+++ test/InMemoryInputConfig.java
@@ -13,6 +13,7 @@ public class InMemoryInputConfig implements InputConfig {
 
 	// When possible, make things final for immutability
 	private final List<Integer> inputs = new ArrayList<>();
+	private String fileName;
 	
 	// Convenience overloading - the ... creates a variable length input. Check out ComputeEngineIntegrationTest for how to use this/why it's nice
 	public InMemoryInputConfig(int... inputs) {
@@ -24,6 +25,9 @@ public class InMemoryInputConfig implements InputConfig {
 	public InMemoryInputConfig(Collection<Integer> inputs) {
 		this.inputs.addAll(inputs);
 	}
+	public InMemoryInputConfig(String fileName) {
+        	this.fileName = fileName;
+    	}
 
 	// Note that this means the class isn't actually immutable, because we're
 	// returning the actual inputs; to be truly immutable, we would either use an
@@ -31,5 +35,9 @@ public class InMemoryInputConfig implements InputConfig {
 	// but since this is test code, YOLO
 	public List<Integer> getInputs() {
 		return inputs;
+	}
+
+	public String getFileName() {
+		return fileName;
 	}
 }
