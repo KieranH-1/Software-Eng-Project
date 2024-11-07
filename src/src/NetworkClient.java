8c9d9b32ebe06f0d8ac12f55c26291b0f4c7cc76
MollyTSlater <mollytslater@gmail.com>
2024-11-06 21:43:26 -0500

    Testing Adding Proto and corresponding Java file

---
+++ src/src/NetworkClient.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.util.Scanner;
+
+public class NetworkClient {
+	
+	 public static void main(String[] args) throws Exception {
+		 Scanner sc = new Scanner(System.in);
+		 
+		 System.out.println("Please enter a txt file or type numbers: ");
+		 
+		 System.out.println("Please enter an output file: ");
+		 
+		 System.out.println("Please enter a txt file or type numbers: ");
+		 
+		 
+	 }
+}
