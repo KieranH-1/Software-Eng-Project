b9056562a7a04a4efdd9d4b942b80dbacc672cad
MollyTSlater <mollytslater@gmail.com>
2024-12-09 11:55:22 -0500

    import change, added full path

--- src/src/NetworkAPIExtension.java
+++ src/src/NetworkAPIExtension.java
@@ -1,12 +1,12 @@
 package src;
 
-import io.grpc.ComputeServiceGrpc.ComputeServiceImplBase;
+import generated.source.proto.main.grpc.src.ComputeServiceGrpc.ComputeServiceImplBase;
 
 public class NetworkAPIExtension extends ComputeServiceImplBase{
 // 
 	public void performComputation(src.NetworkAPI.InputRequest request,
 	        io.grpc.stub.StreamObserver<src.NetworkAPI.InputResponse> responseObserver) {
 	     //replace w actual implementation
-		//io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall(getPerformComputationMethod(), responseObserver);
+		io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall(getPerformComputationMethod(), responseObserver);
 	    }
 }
