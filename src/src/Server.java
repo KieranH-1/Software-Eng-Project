6d7ce3fec495d4b0d743de14e96e8df2f8e5d697
jasonmontas <jason.montas24@gmail.com>
2024-12-13 18:01:59 -0500

    Completed TODO's for Server.java and TestServer.java

--- src/src/Server.java
+++ src/src/Server.java
@@ -1,13 +1,25 @@
 package src;
+
+import java.nio.ByteBuffer;
+
 public class Server {
 
 	public byte[] addValues(byte[] data) {
 		// TODO: set 'request' to a value read from 'data'
-		Request request = null;
+
+		ByteBuffer buffer = ByteBuffer.wrap(data);
+		int value1 = buffer.getInt();
+		int value2 = buffer.getInt();
+
+		Request request = new Request(value1, value2);
 		
 		int result = request.getValue1() + request.getValue2();
 		
 		// TODO: write the result and a success code to a byte array, and return that
-		return null;
+		ByteBuffer responseBuffer = ByteBuffer.allocate(8);
+		responseBuffer.putInt(result);
+		responseBuffer.putInt(0);
+
+		return responseBuffer.array();
 	}
 }
