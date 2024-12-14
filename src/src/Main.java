a0b36782ecbd06993707a81bb37256e97b3f515e
jasonmontas <jason.montas24@gmail.com>
2024-12-13 19:16:16 -0500

    Improved CLI and fixed FAILURE error

--- src/src/Main.java
+++ src/src/Main.java
@@ -12,19 +12,33 @@ public class Main {
         DataStore data = new DataStoreImpl();
         ComputationCoordinator coordinator = new CoordinatorImpl(data, engine);
 
-        System.out.print("Please specify a file you would like to use as your input: ");
-        String inputPath = sc.nextLine();
-        
-        System.out.print("Do the same for an output file: ");
-        String outputPath = sc.nextLine();
+        try {
+            System.out.print("Please specify a file you would like to use as your input: ");
+            String inputPath = sc.nextLine();
+            File inputFile = new File(inputPath);
+            if(!inputFile.exists() || !inputFile.isFile()) {
+                System.out.println("Error: Input file does not exist or is not a valid file.");
+                return;
+            }
 
-        FileInputConfig input = new FileInputConfig(inputPath);
-        FileOutputConfig output = new FileOutputConfig(outputPath);
-        ComputeRequest request = new ComputeRequest(input, output);
-        coordinator.compute(request);
+            System.out.print("Enter an output file: ");
+            String outputPath = sc.nextLine();
+            File outputFile = new File(outputPath);
 
-        ComputeResult result = coordinator.compute(request);
-        System.out.println(result.getStatus());
+            FileInputConfig input = new FileInputConfig(inputPath);
+            FileOutputConfig output = new FileOutputConfig(outputPath);
+            ComputeRequest request = new ComputeRequest(input, output);
+            
+            ComputeResult result = coordinator.compute(request);
+            System.out.println("Computation Result: " + result.getStatus());
+
+        } catch (Exception e) {
+            System.out.println("An error occurred during computation: ");
+            e.printStackTrace();
+        } finally {
+            coordinator.close();
+            sc.close();
+        }
 
 
 
